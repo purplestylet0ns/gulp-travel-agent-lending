b416b229e8878952fcf6df323f3cce5a9422d967
purplestylet0ns <antonprotsysyn@gmail.com>
2023-10-31 19:33:49 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# gulp-travel-agent-lending
+# Для запуску проекта використовуй такі команди як gulp / gulp build 
---
+++ src/js/index.js
@@ -0,0 +1,12 @@
+import mobileNav from "./modules/mobile-nav.js";
+import swiper from "./modules/swiper-popular.js";
+import autoComplete from "./modules/autoComplete";
+import easepick from "./modules/easepick.js";
+import scrollReveal from "./modules/scrollReveal.js";
+import swiperTestimonials from "./modules/swiper-testimonials.js";
+
+mobileNav();
+autoComplete();
+easepick();
+swiper();
+scrollReveal();
